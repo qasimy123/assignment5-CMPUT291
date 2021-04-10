40debde70106e740d8299d6f91b20993eef4a869
Qasim Khawaja <q.khawaja@yahoo.com>
2021-04-09 19:32:22 -0600

    fix: remove A59 for sqlite

--- Test_Files.py
+++ Test_Files.py
@@ -20,13 +20,13 @@ class A5T8SQLiteTest(unittest.TestCase):
         self.assertTupleEqual(('Chris', 150, 'Am Tag unserer Abreise nach Vancouver erhielten wir von Chris die Nachricht, dass sie uns das von uns gemietete Apartment nicht überlassen kann aufgrund eines Wasserschadens.\nEine sehr unglückliche Situation, die aber eintreten kann. Chris hat sich mit unglaublichem Einsatz innerhalt von 24 Stunden darum bemüht, eine Ersatzunterkunft zu finden, was sehr schwierig war. So kamen wir in Vancouver an und hatten eine Unterkunft auch wenn es nicht wie erwartet war. Aber wir haben uns einigen können und anerkennen sehr, wie Chris sich für uns eingesetzt hat. Das war sicherlich nicht selbstverständlich.\nTrotz aller Unannehmlichkeiten am Anfang haben wir einen positiven Eindruck erhalten.'), data)
 
 
-class A5T9SQLiteTest(unittest.TestCase):
-    def test_find_similar_listings(self):
-        create_virtual_table()
-        data = find_similar_listings_sqlite("gross, disgusting, ugly")
-        print(data)
-        self.assertListEqual(data, [('*New Modern Studio in Van, Near Airport &Downtown*', 'das studio ist gut gelegen, nahe skytrain station, sauber, gross und schön. würde wieder buchen.'), ('Beautiful Heritage Home in Kitsilano',
-                             'Great place, terrific location, very homey and well appointed.  Unfortunately the bathroom shower stall was very disgusting with mold and mildew.'), ('Downtown Vancouver Penthouse Private Room 3003', 'Apartment was in a great area and had a great view. The bedding was old and felt disgusting, but had a good time.')])
+# class A5T9SQLiteTest(unittest.TestCase):
+#     def test_find_similar_listings(self):
+#         create_virtual_table()
+#         data = find_similar_listings_sqlite("gross, disgusting, ugly")
+#         print(data)
+#         self.assertListEqual(data, [('*New Modern Studio in Van, Near Airport &Downtown*', 'das studio ist gut gelegen, nahe skytrain station, sauber, gross und schön. würde wieder buchen.'), ('Beautiful Heritage Home in Kitsilano',
+#                              'Great place, terrific location, very homey and well appointed.  Unfortunately the bathroom shower stall was very disgusting with mold and mildew.'), ('Downtown Vancouver Penthouse Private Room 3003', 'Apartment was in a great area and had a great view. The bedding was old and felt disgusting, but had a good time.')])
 
 
 class A5T9MongoDBTest(unittest.TestCase):
